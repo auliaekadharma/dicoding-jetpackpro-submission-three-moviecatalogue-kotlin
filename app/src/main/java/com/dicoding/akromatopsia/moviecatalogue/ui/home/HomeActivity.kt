fb746ec35c3bed797f908945fb4f91b718a86937
auliaekadharma <auliaekadharma@gmail.com>
2021-05-18 18:12:51 +0700

    Bottom Navigation implemented

--- app/src/main/java/com/dicoding/akromatopsia/moviecatalogue/ui/home/HomeActivity.kt
+++ app/src/main/java/com/dicoding/akromatopsia/moviecatalogue/ui/home/HomeActivity.kt
@@ -2,7 +2,13 @@ package com.dicoding.akromatopsia.moviecatalogue.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dicoding.akromatopsia.moviecatalogue.R
 import com.dicoding.akromatopsia.moviecatalogue.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -12,9 +18,21 @@ class HomeActivity : AppCompatActivity() {
         val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(activityHomeBinding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
-        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_movies, R.id.nav_tvshows, R.id.nav_favorites
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+
+//        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+//        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
+//        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
 
         supportActionBar?.elevation = 0f
 
