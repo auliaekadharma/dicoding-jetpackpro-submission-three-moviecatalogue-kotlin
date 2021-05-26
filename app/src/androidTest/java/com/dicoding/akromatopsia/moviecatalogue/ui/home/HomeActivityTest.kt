0970cfcb7ab4585079463a066326792eb4414fbd
auliaekadharma <auliaekadharma@gmail.com>
2021-05-26 16:15:58 +0700

    Instrumentation Test completed

--- app/src/androidTest/java/com/dicoding/akromatopsia/moviecatalogue/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/akromatopsia/moviecatalogue/ui/home/HomeActivityTest.kt
@@ -2,11 +2,14 @@ package com.dicoding.akromatopsia.moviecatalogue.ui.home
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.dicoding.akromatopsia.moviecatalogue.R
@@ -38,17 +41,13 @@ class HomeActivityTest {
 
     @Test
     fun loadMovies() {
-        //delayTwoSecond()
         onView(withId(R.id.rv_movie)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
     }
 
     @Test
     fun loadDetailMovie() {
-        //delayTwoSecond()
-        //delayTwoSecond()
         onView(withId(R.id.rv_movie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        //delayTwoSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyMovie[0].title)))
         onView(withId(R.id.text_year)).check(matches(isDisplayed()))
@@ -61,22 +60,22 @@ class HomeActivityTest {
         onView(withId(R.id.text_duration)).check(matches(withText(dummyMovie[0].duration)))
         onView(withId(R.id.text_description)).check(matches(isDisplayed()))
         onView(withId(R.id.text_description)).check(matches(withText(dummyMovie[0].description)))
+
+        onView(withId(R.id.action_favorite)).perform(click())
+
     }
 
     @Test
     fun loadTvshows() {
-        //delayTwoSecond()
-        onView(withText("TV Shows")).perform(click())
+        onView(withId(R.id.nav_tvshows)).perform(click())
         onView(withId(R.id.rv_tvshow)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvshow.size))
     }
 
     @Test
     fun loadDetailTvshow() {
-        //delayTwoSecond()
-        onView(withText("TV Shows")).perform(click())
+        onView(withId(R.id.nav_tvshows)).perform(click())
         onView(withId(R.id.rv_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        //delayTwoSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyTvshow[0].title)))
         onView(withId(R.id.text_year)).check(matches(isDisplayed()))
@@ -87,14 +86,60 @@ class HomeActivityTest {
         onView(withId(R.id.text_duration)).check(matches(withText(dummyTvshow[0].duration)))
         onView(withId(R.id.text_description)).check(matches(isDisplayed()))
         onView(withId(R.id.text_description)).check(matches(withText(dummyTvshow[0].description)))
+
+        onView(withId(R.id.action_favorite)).perform(click())
     }
 
-    // Removed due to IdlingResource implementation
-//    private fun delayTwoSecond() {
-//        try {
-//            Thread.sleep(2000)
-//        } catch (e: InterruptedException) {
-//            e.printStackTrace()
-//        }
-//    }
+    @Test
+    fun loadFavoriteMovies() {
+        onView(withId(R.id.nav_favorites)).perform(click())
+        onView(withId(R.id.rv_fav_movie)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_fav_movie)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyMovie.size))
+    }
+
+    @Test
+    fun loadDetailFavoriteMovie() {
+        onView(withId(R.id.nav_favorites)).perform(click())
+        onView(withId(R.id.rv_fav_movie)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(dummyMovie[0].title)))
+        onView(withId(R.id.text_year)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_year)).check(matches(withText(dummyMovie[0].year)))
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_date)).check(matches(withText(dummyMovie[0].releaseDate)))
+        onView(withId(R.id.text_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_genre)).check(matches(withText(dummyMovie[0].genres)))
+        onView(withId(R.id.text_duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_duration)).check(matches(withText(dummyMovie[0].duration)))
+        onView(withId(R.id.text_description)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_description)).check(matches(withText(dummyMovie[0].description)))
+
+    }
+
+    @Test
+    fun loadFavoriteTvshows() {
+        onView(withId(R.id.nav_favorites)).perform(click())
+        onView(withText("TV SHOWS")).perform(click())
+        onView(withId(R.id.rv_fav_tvshow)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_fav_tvshow)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyTvshow.size))
+    }
+
+    @Test
+    fun loadDetailFavoriteTvshow() {
+        onView(withId(R.id.nav_favorites)).perform(click())
+        onView(withText("TV SHOWS")).perform(click())
+        onView(withId(R.id.rv_fav_tvshow)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_title)).check(matches(withText(dummyTvshow[0].title)))
+        onView(withId(R.id.text_year)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_year)).check(matches(withText(dummyTvshow[0].year)))
+        onView(withId(R.id.text_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_genre)).check(matches(withText(dummyTvshow[0].genres)))
+        onView(withId(R.id.text_duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_duration)).check(matches(withText(dummyTvshow[0].duration)))
+        onView(withId(R.id.text_description)).check(matches(isDisplayed()))
+        onView(withId(R.id.text_description)).check(matches(withText(dummyTvshow[0].description)))
+
+    }
+
 }
